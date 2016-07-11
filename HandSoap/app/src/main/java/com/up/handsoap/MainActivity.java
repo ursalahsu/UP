27b9e14359a64f96c7c54ac8d38058900cf62311
ursalahsu <ursala99@gmail.com>
2016-07-12 00:58:43 +0800

    add Oil class

--- HandSoap/app/src/main/java/com/up/handsoap/MainActivity.java
+++ HandSoap/app/src/main/java/com/up/handsoap/MainActivity.java
@@ -3,6 +3,8 @@ package com.up.handsoap;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +18,10 @@ public class MainActivity extends AppCompatActivity {
         oilAddButton = (Button) findViewById(R.id.addButton);
     }
 
-    private void oilsAdd(){
+    public void addOils(View view){
+//        Log.d("Debug","click");
         Intent intent = new Intent();
-
+        intent.setClass(this,OilsActivity.class);
+        startActivity(intent);
     }
 }
--- HandSoap/app/src/main/java/com/up/handsoap/OilsActivity.java
+++ HandSoap/app/src/main/java/com/up/handsoap/OilsActivity.java
@@ -1,18 +1,27 @@
 package com.up.handsoap;
 
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OilsActivity extends AppCompatActivity {
     ListView oilsListview;
     String[] oilsArray;
+    List<Oil> oils = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +29,36 @@ public class OilsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_oils);
 
         oilsListview = (ListView) findViewById(R.id.oilsListView);
+        //取得油品清單
         oilsArray = getResources().getStringArray(R.array.oilsName);
         ArrayAdapter<String>  adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,oilsArray);
+        oilsListview.setAdapter(adapter);
+        //設定可多選
+        oilsListview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        oilsListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Oil oil = new Oil();
+                oil.setName(oilsArray[position]);
+                oils.add(oil);
+            }
+        });
     }
 
     private void checkReturn(){
-
+        Intent intent=new Intent();
+        JSONArray jsonArray = new JSONArray();
+        for(Oil oil:oils){
+            JSONObject jsonObject=oil.getJsonObject();
+            jsonArray.put(jsonObject);
+        }
+        intent.putExtra("oilsList",jsonArray.toString());
+        setResult(RESULT_OK,intent);
+        finish();
     }
 
     private void checkCancel(){
-
+        setResult(RESULT_CANCELED);
+        finish();
     }
 }
